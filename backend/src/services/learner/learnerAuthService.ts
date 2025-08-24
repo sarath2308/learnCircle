f95b4015bebeb272376be537453fdcd8a9b59c09
sarath vp <sarathvp1546@gmail.com>
2025-08-24 19:10:57 +0530

    learnerAuth service structure

---
+++ backend/src/services/learner/learnerAuthService.ts
@@ -0,0 +1,42 @@
+
+
+
+
+export interface IlearnerAuthService
+{
+    signup:(name:string,email:string,password:string)=>Promise<void>;
+    login:(email:string,password:string)=>Promise<void>;
+    forgotPassword:(email:string)=>Promise<void>
+    resetPassword:(id:string,newPassword:string)=>Promise<void>
+    verifyOtp?:(email:string,otp:string)
+
+}
+
+export class LearnerAuthService implements IlearnerAuthService
+{
+    constructor()
+    {
+
+    }
+    async signup(name:string,email:string,password:string)
+    {
+
+    }
+
+    async login(email:string,password:string)
+    {
+
+    }
+    async forgotPassword(email: string) 
+    {
+
+    }
+    async resetPassword(id:string,newPassword:string)
+    {
+
+    }
+    async verifyOtp(email:string,otp:string)
+    {
+
+    }
+}
