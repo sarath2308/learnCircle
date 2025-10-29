ce96edae93fbc5f25cdc0b6efcd3524cdf7b4377
Sarath vp <162009289+sarath2308@users.noreply.github.com>
2025-10-29 14:06:51 +0530

    dependency moved to seperate entry - alignment corrected

--- backend/src/common/routes/auth.entry.route.ts
+++ backend/src/common/routes/auth.entry.route.ts
@@ -6,14 +6,13 @@ import { IAuthController } from "../interface";
 import { authRoutes } from "./auth.routes";
 import { refreshRoutes } from "./refresh.routes";
 
-export function authEntryRoute()
-{
-    const router =  Router();
-    const refreshController = container.get<IRefreshController>(TYPES.IRefreshController);
-    const authController =  container.get<IAuthController>(TYPES.IAuthController);
+export function authEntryRoute() {
+  const router = Router();
+  const refreshController = container.get<IRefreshController>(TYPES.IRefreshController);
+  const authController = container.get<IAuthController>(TYPES.IAuthController);
 
-    router.use("/",authRoutes(authController));
-    router.use("/refresh-token", refreshRoutes(refreshController));
+  router.use("/", authRoutes(authController));
+  router.use("/refresh-token", refreshRoutes(refreshController));
 
-    return router;
+  return router;
 }
