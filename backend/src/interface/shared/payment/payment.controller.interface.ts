da211c1399d73d0ff5008d45e508df0fd3bafe07
Sarath vp <162009289+sarath2308@users.noreply.github.com>
2026-02-25 23:20:56 +0530

    fix: payment route position change
    -webhook route will work before payment

--- backend/src/interface/shared/payment/payment.controller.interface.ts
+++ backend/src/interface/shared/payment/payment.controller.interface.ts
@@ -1,7 +1,7 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { IAuthRequest } from "../auth/auth.request.interface";
 
 export interface IPaymentController {
-  handleWebHook: (req: IAuthRequest, res: Response) => Promise<void>;
+  handleWebHook: (req: Request, res: Response) => Promise<void>;
   checkPaymentStatus: (req: IAuthRequest, res: Response) => Promise<void>;
 }
